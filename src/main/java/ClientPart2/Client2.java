d825ac4fac87a945f9253cdd1b4145f926ed422c
HyungSuk Lee <hslee9405@gmail.com>
2023-11-04 22:55:37 -0700

    assignment2

--- src/main/java/ClientPart2/Client2.java
+++ src/main/java/ClientPart2/Client2.java
@@ -1,8 +1,8 @@
 package ClientPart2;
 
-import com.opencsv.CSVWriter;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.File;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -20,11 +21,17 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+
 public class Client2 {
-  private static final int THREAD_COUNT = 10;
-  private static LinkedBlockingQueue<RequestDetail> requestDetails = new LinkedBlockingQueue<>();
+
+  private static LinkedBlockingQueue<RequestDetail> getRequestsDetails = new LinkedBlockingQueue<>();
+  private static LinkedBlockingQueue<RequestDetail> postRequestsDetails = new LinkedBlockingQueue<>();
+
   private static final AtomicInteger atomicInteger = new AtomicInteger(0);
-  private static final HttpClient httpClient = HttpClients.createDefault();
+  private static final AtomicInteger successfulRequests = new AtomicInteger(0);
+  private static final AtomicInteger failedRequests = new AtomicInteger(0);
+  private static final AtomicReference<String> lastAlbumId = new AtomicReference<>(
+      ""); // Changed to AtomicReference for String type
 
   public static void main(String[] args) {
     if (args.length != 4) {
@@ -36,77 +43,96 @@ public class Client2 {
     int numThreadGroups = Integer.parseInt(args[1]);
     int delay = Integer.parseInt(args[2]);
     String serverUri = args[3];
+    int threadPoolSize =
+        numThreadGroups * threadGroupSize; // Calculate the appropriate thread pool size
 
-    int threadPoolSize = numThreadGroups * threadGroupSize; // Calculate the appropriate thread pool size
+    long startTime = System.currentTimeMillis();
 
-    ExecutorService executorService1 = Executors.newFixedThreadPool(threadPoolSize);
     try {
+      ExecutorService groupExecutorService = Executors.newFixedThreadPool(numThreadGroups);
+      CountDownLatch groupLatch = new CountDownLatch(numThreadGroups);
 
-      long startTime = System.currentTimeMillis();
-      CountDownLatch latch = new CountDownLatch(threadPoolSize);
+      for (int group = 0; group < numThreadGroups; group++) {
+        final int currentGroup = group;
 
-      for (int group = 1; group <= numThreadGroups; group++) {
-        executorService1.submit(() -> {
-          ExecutorService executorService2 = Executors.newFixedThreadPool(threadPoolSize);
-          for (int i = 0; i < threadGroupSize; i++) {
-            executorService2.submit(() -> {
-              try {
+//        CountDownLatch latch = new CountDownLatch(threadGroupSize);
+        groupExecutorService.submit(() -> {
+          try {
+            ExecutorService threadExecutorService = Executors.newFixedThreadPool(threadGroupSize);
+            CountDownLatch threadLatch = new CountDownLatch(threadGroupSize);
+            for (int i = 0; i < threadGroupSize; i++) {
+              threadExecutorService.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
                 HttpClient httpClient = HttpClients.createDefault();
-                for(int k = 0; k < 1000; k++){
+                for (int k = 0; k < 1000; k++) {
                   atomicInteger.addAndGet(performPostRequest(serverUri, httpClient));
                   atomicInteger.addAndGet(performGetRequest(serverUri, httpClient));
                 }
-              } finally {
-                latch.countDown();
-              }
-            });
-          }
-          executorService2.shutdown();
-          try {
-            Thread.sleep(delay * 1000); // Convert delay from seconds to milliseconds
+                threadLatch.countDown();
+              });
+            }
+            threadExecutorService.shutdown();
+            threadLatch.await(); // Wait for all threads in group to finish
+
           } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-          }
-          while (!executorService2.isTerminated()) {
+            Thread.currentThread().interrupt();
+          } finally {
+            groupLatch.countDown();
           }
         });
+        if (currentGroup < numThreadGroups - 1) {
+          try {
+            Thread.sleep(delay * 1000); // Delay before starting next group
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+        }
       }
-
-      executorService1.shutdown();
-      latch.await();
-      while (!executorService1.isTerminated()) {
-      }
-
+      groupExecutorService.shutdown();
+      groupLatch.await(); // Wait for all thread groups to finis
       long endTime = System.currentTimeMillis();
 
       List<Long> latencies = new ArrayList<>();
-      writeRecordsToCSV(requestDetails, "statistics.csv");
-      for (int i = 0; i < requestDetails.size(); i++) {
-        RequestDetail requestDetail = requestDetails.poll();
-        if (requestDetail != null) {
-          latencies.add(requestDetail.getLatency());
-        }
+      List<Long> getLatencies = new ArrayList<>();
+      List<Long> postLatencies = new ArrayList<>();
+//      writeRecordsToCSV(requestDetails, "statistics.csv");
+      for (RequestDetail detail : getRequestsDetails) {
+        getLatencies.add(detail.getLatency());
       }
-
-
+      for (RequestDetail detail : postRequestsDetails) {
+        postLatencies.add(detail.getLatency());
+      }
       long wallTime = (endTime - startTime) / 1000; // Convert to seconds
       double throughput = (double) atomicInteger.get() / wallTime;
 
       System.out.println("Wall Time: " + wallTime + " seconds");
       System.out.println("Throughput: " + throughput + " requests/second");
-      long meanResponseTime = getMean(latencies);
-      long medianResponseTime = getMedian(latencies);
-      long p99ResponseTime = getPercentile(latencies, 99);
-      long minResponseTime = Collections.min(latencies);
-      long maxResponseTime = Collections.max(latencies);
 
-      // Display response time statistics
-      System.out.println("Mean Response Time: " + meanResponseTime + " milliseconds");
-      System.out.println("Median Response Time: " + medianResponseTime + " milliseconds");
-      System.out.println("P99 Response Time: " + p99ResponseTime + " milliseconds");
-      System.out.println("Min Response Time: " + minResponseTime + " milliseconds");
-      System.out.println("Max Response Time: " + maxResponseTime + " milliseconds");
+      // Calculate statistics for GET requests
+      long getMeanResponseTime = getMean(getLatencies);
+      long getMedianResponseTime = getMedian(getLatencies);
+      long getP99GetResponseTime = getPercentile(getLatencies, 99);
+      long getMinResponseTime = Collections.min(getLatencies);
+      long getMaxResponseTime = Collections.max(getLatencies);
 
+      // Calculate statistics for POST requests
+      long postMeanResponseTime = getMean(postLatencies);
+      long postMedianResponseTime = getMedian(postLatencies);
+      long getP99PostResponseTime = getPercentile(postLatencies, 99);
+      long postMinResponseTime = Collections.min(postLatencies);
+      long postMaxResponseTime = Collections.max(postLatencies);
+
+      System.out.println("GET Mean Response Time: " + getMeanResponseTime + " milliseconds");
+      System.out.println("GET Median Response Time: " + getMedianResponseTime + " milliseconds");
+      System.out.println("GET P99 Response Time: " + getP99GetResponseTime + " milliseconds");
+      System.out.println("GET Min Response Time: " + getMinResponseTime + " milliseconds");
+      System.out.println("GET Max Response Time: " + getMaxResponseTime + " milliseconds");
+
+      System.out.println("POST Mean Response Time: " + postMeanResponseTime + " milliseconds");
+      System.out.println("POST Median Response Time: " + postMedianResponseTime + " milliseconds");
+      System.out.println("POST P99 Response Time: " + getP99PostResponseTime + " milliseconds");
+      System.out.println("POST Min Response Time: " + postMinResponseTime + " milliseconds");
+      System.out.println("POST Max Response Time: " + postMaxResponseTime + " milliseconds");
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -114,67 +140,112 @@ public class Client2 {
 
   private static int performPostRequest(String serverUri, HttpClient httpClient) {
     int requestCount = 1; // Keep track of the number of requests
+    int retryAttempts = 0; // Counter for retry attempts
 
     long startTime = System.currentTimeMillis(); // Capture start time
 
-    HttpPost postRequest = new HttpPost(serverUri + "/albums");
+    while (retryAttempts < 5) { // Retry up to 5 times
 
-    try {
-      MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-      File imageFile = new File("nmtb.png");
+      HttpPost postRequest = new HttpPost(serverUri + "/albums");
 
-      builder.addBinaryBody("image", imageFile);
-      builder.addTextBody("profile",
-          "{\"artist\":\"John Doe\",\"title\":\"Greatest Hits\",\"year\":\"2023\"}");
+      try {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        File imageFile = new File("nmtb.png");
 
-      HttpEntity multipart = builder.build();
-      postRequest.setEntity(multipart);
-      HttpResponse response = httpClient.execute(postRequest);
+        builder.addBinaryBody("image", imageFile);
+        builder.addTextBody("artist", "John Doe");
+        builder.addTextBody("title", "Greatest Hits");
+        builder.addTextBody("year", "2023");
 
-      int statusCode = response.getStatusLine().getStatusCode();
+        HttpEntity multipart = builder.build();
+        postRequest.setEntity(multipart);
 
-      if (!(statusCode == 200 ||statusCode == 201)) {
-        requestCount++;
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-          String result = EntityUtils.toString(entity);
+        HttpResponse response = httpClient.execute(postRequest);
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        if (statusCode == 200 || statusCode == 201) {
+          // If successful, exit the loop
+          HttpEntity entity = response.getEntity();
+          String responseBody = EntityUtils.toString(entity);
+          JsonObject responseJson = JsonParser.parseString(responseBody).getAsJsonObject();
+          if (responseJson.has("albumId")) {
+            lastAlbumId.set(responseJson.get("albumId").getAsString()); // Store the new album ID
+          }
+          EntityUtils.consume(entity);
+          EntityUtils.consume(response.getEntity());
+
+          successfulRequests.incrementAndGet(); // Increment successful request counter
+          long endTime = System.currentTimeMillis(); // Capture end time
+          long latency = endTime - startTime; // Calculate latency
+          postRequestsDetails.add(new RequestDetail(startTime, "POST", latency, statusCode));
+
+          break;
+        } else if (statusCode >= 400 && statusCode <= 599) {
+          // Increment retryAttempts if 4XX or 5XX response code
+          if (retryAttempts == 4) {
+            failedRequests.incrementAndGet();
+          }
+          retryAttempts++;
+          requestCount++;
         }
+
+      } catch (Exception e) {
+        if (retryAttempts == 4) {
+          failedRequests.incrementAndGet();
+        }
+        e.printStackTrace();
+        retryAttempts++;
+
       }
-
-      long endTime = System.currentTimeMillis(); // Capture end time
-      long latency = endTime - startTime; // Calculate latency
-      requestDetails.add(new RequestDetail(startTime, "POST", latency, statusCode)); // Add request details to the queue
-
-    } catch (Exception e) {
-      e.printStackTrace();
     }
     return requestCount;
   }
 
   private static int performGetRequest(String serverUri, HttpClient httpClient) {
     int requestCount = 1; // Keep track of the number of requests
+    int retryAttempts = 0; // Counter for retry attempts
 
     long startTime = System.currentTimeMillis(); // Capture start time
 
-    HttpGet getRequest = new HttpGet(serverUri + "/albums/123");
-
-    try {
-      HttpResponse response = httpClient.execute(getRequest);
-      int statusCode = response.getStatusLine().getStatusCode();
-
-      if (!(statusCode == 200 ||statusCode == 201)) {
-        HttpEntity entity = response.getEntity();
-        requestCount++;
+    while (retryAttempts < 5) { // Retry up to 5 times
+      String currentAlbumId = lastAlbumId.get();
+      if (currentAlbumId.isEmpty()) {
+        System.err.println("No valid album ID available for GET request.");
+        break; // Skip the GET request if no valid ID is available
       }
+      HttpGet getRequest = new HttpGet(serverUri + "/albums/" + currentAlbumId);
 
-      long endTime = System.currentTimeMillis(); // Capture end time
-      long latency = endTime - startTime; // Calculate latency
-      requestDetails.add(new RequestDetail(startTime, "GET", latency, statusCode)); // Add request details to the queue
+      try {
+        HttpResponse response = httpClient.execute(getRequest);
+        EntityUtils.consume(response.getEntity());
 
-    } catch (Exception e) {
-      e.printStackTrace();
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        if (statusCode == 200 || statusCode == 201) {
+          successfulRequests.incrementAndGet(); // Increment successful request counter
+          long endTime = System.currentTimeMillis(); // Capture end time
+          long latency = endTime - startTime; // Calculate latency
+          getRequestsDetails.add(new RequestDetail(startTime, "GET", latency, statusCode));
+
+          break;
+        } else if (statusCode >= 400 && statusCode <= 599) {
+          // Increment retryAttempts if 4XX or 5XX response code
+          if (retryAttempts == 4) {
+            failedRequests.incrementAndGet();
+          }
+          retryAttempts++;
+          requestCount++;
+        }
+
+      } catch (Exception e) {
+        if (retryAttempts == 4) {
+          failedRequests.incrementAndGet();
+        }
+        e.printStackTrace();
+        retryAttempts++; // Increment retry attempts on exception
+
+      }
     }
-
     return requestCount;
   }
 
@@ -203,6 +274,7 @@ public class Client2 {
   }
 
   private static class RequestDetail {
+
     private final long startTime;
     private final String requestType;
     private final long latency;
@@ -217,29 +289,6 @@ public class Client2 {
 
     public long getLatency() {
       return latency;
-    }
-  }
-  private static void writeRecordsToCSV(LinkedBlockingQueue<RequestDetail> requestDetails, String filename) {
-    try {
-      CSVWriter writer = new CSVWriter(new FileWriter(filename));
-
-      // Adding header to the CSV
-      String[] header = {"Start Time", "Request Type", "Latency", "Response Code"};
-      writer.writeNext(header);
-
-      for (RequestDetail requestDetail : requestDetails) {
-        String[] record = {
-            String.valueOf(requestDetail.startTime),
-            requestDetail.requestType,
-            String.valueOf(requestDetail.latency),
-            String.valueOf(requestDetail.statusCode)
-        };
-        writer.writeNext(record);
-      }
-
-      writer.close();
-    } catch (Exception e) {
-      e.printStackTrace();
     }
   }
 }
